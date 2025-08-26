a332fd67411bede4164478e659cb4e5a04bc630d
Anil Kumar <kuragayalaanil007@gmail.com>
2025-08-26 17:59:32 +0530

    add Product

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Admin from "./pages/Admin/Admin";
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Admin />
+    </div>
+  );
+};
+
+export default App;
